53b8e67dacb49bf43ba0f3c2433349baba4b3245
MZabell <marcuszabell@outlook.com>
2024-02-09 20:28:27 +0100

    New dependencies added ObjectRecog.java changed to match new API

--- src/main/java/org/detection/ObjectRecog.java
+++ src/main/java/org/detection/ObjectRecog.java
@@ -1,50 +1,63 @@
 package org.detection;
 
-import nu.pattern.OpenCV;
-import org.opencv.core.*;
-import org.opencv.highgui.HighGui;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.videoio.VideoCapture;
+import org.bytedeco.javacv.*;
+import org.bytedeco.opencv.opencv_core.*;
+import org.bytedeco.opencv.opencv_imgproc.Vec3fVector;
+
+import javax.swing.*;
+
+import static org.bytedeco.opencv.global.opencv_core.*;
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
 
 public class ObjectRecog {
 
     int scan() {
-        OpenCV.loadLocally();
 
-        VideoCapture capture = new VideoCapture("src/main/resources/video.mp4");
+        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber("src/main/resources/video.mp4");
+        CanvasFrame canvas = new CanvasFrame("Object Detection", CanvasFrame.getDefaultGamma() / grabber.getGamma());
+        canvas.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
+        Mat image;
+        Mat gray = new Mat();
+        Vec3fVector circles = new Vec3fVector();
+        Mat lower = new Mat(1, 1, CV_8UC4, new Scalar(130, 130, 130, 0));
+        Mat upper = new Mat(1, 1, CV_8UC4, new Scalar(255, 255, 255, 0));
+        try {
+            grabber.start();
+            Frame frame;
+            while ((frame = grabber.grabImage()) != null) {
+                image = converter.convert(frame);
+                cvtColor(image, gray, CV_BGR2GRAY);
+                GaussianBlur(gray, gray, new Size(7, 7), 2);
 
-        Mat image = new Mat();
-        Mat mask = new Mat();
-        Scalar lower = new Scalar(130, 130, 130);
-        Scalar upper = new Scalar(255, 255, 255);
-        Mat circles = new Mat();
+                HoughCircles(gray, circles, HOUGH_GRADIENT, 1, 20, 200, 40, 1, 0);
+                for (int i = 0; i < (int) circles.size(); i++) {
+                    Point3f circle = circles.get(i);
+                    Point center = new Point(Math.round(circle.get(0)), Math.round(circle.get(1)));
+                    int radius = Math.round(circle.get(2));
 
-        while (capture.read(image)) {
-            Imgproc.cvtColor(image, mask, Imgproc.COLOR_BGR2GRAY);
+                    Rect roi = new Rect(center.x() - radius, center.y() - radius, radius * 2, radius * 2);
 
-            Imgproc.GaussianBlur(mask, mask, new Size(7, 7), 2);
-
-            Imgproc.HoughCircles(mask, circles, Imgproc.HOUGH_GRADIENT, 1.0, 25, 60, 40, 1, 150);
-            for (int i = 0; i < circles.cols(); i++) {
-                double[] circle = circles.get(0, i);
-                Point center = new Point(Math.round(circle[0]), Math.round(circle[1]));
-                int radius = (int) Math.round(circle[2]);
-
-                Rect roi = new Rect((int) center.x - radius, (int) center.y - radius, radius * 2, radius * 2);
-
-                Mat roiMat = new Mat(image, roi);
-                Mat color = new Mat(roiMat.size(), CvType.CV_8UC3, Core.mean(roiMat));
-                Core.inRange(color, lower, upper, color);
-                if (Core.countNonZero(color) > 0) {
-                    Imgproc.circle(image, center, 1, new Scalar(0, 100, 100), 3);
-                    Imgproc.circle(image, center, radius, new Scalar(0, 255, 0), 2);
+                    Mat roiMat;
+                    try {
+                        roiMat = new Mat(image, roi);
+                    } catch (RuntimeException e) {
+                        continue;
+                    }
+                    Mat color = new Mat(roiMat.size(), CV_8UC3, mean(roiMat));
+                    inRange(color, lower, upper, color);
+                    if (countNonZero(color) > 0) {
+                        circle(image, center, radius, new Scalar(0, 255, 0, 0), 1, LINE_8, 0);
+                    }
                 }
+                frame = converter.convert(image);
+                canvas.showImage(frame);
             }
-
-            HighGui.imshow("Output", image);
-            HighGui.waitKey(1);
+            grabber.stop();
+        } catch (FrameGrabber.Exception e) {
+            e.printStackTrace();
         }
-        capture.release();
+
         return 0;
     }
 }
